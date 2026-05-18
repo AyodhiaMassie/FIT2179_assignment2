360def0e192d65970a0ea5ed23a8d274f91b7df6
AyodhiaMassie <ayodhiamassie92@gmail.com>
2026-05-19 03:03:12 +0800

    added draft layout for six charts

---
+++ main.js
@@ -0,0 +1,3 @@
+vegaEmbed("#chart1", "charts/ch1_malaysia_internet_usage_overtime.vg.json", {
+  actions: false
+}).catch(console.error);
--- vegalite_test_viz.js
+++
@@ -1,7 +0,0 @@
-var vg_1 = "vegalite_test.vg.json";
-
-vegaEmbed("#bar_chart", vg_1)
-    .then(function(result) {
-        console.log("Chart loaded successfully");
-    })
-    .catch(console.error);
